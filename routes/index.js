77532f33880d5f021928e01f1ce948230ad26f9e
arielordf <arielfabio@gmail.com>
2022-09-17 02:54:29 +0000

    update json

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,28 @@ const creadas = {
   "total": 2,
   "list": [
     {
+      "id": 1,
       "imagen": "https://images.unsplash.com/photo-1663167266768-b7384b93b28e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
       "titulo": "Todos por Marcos Fernando",
+      "descripcion": "Fernando es un amigo, padre que en estos momentos dificiles necesita de nosotros",
       "vendidos": 4,
-      "id": 1
+      "premios": [
+        "Una TV",
+        "Una Heladera",
+        "Un termo"
+      ]
     },
     {
+      "id": 2,
       "imagen": "https://images.unsplash.com/photo-1662855802712-7e30ebfbb194?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
       "titulo": "Todos por Maria Antonieta",
+      "descripcion": "Fernando es un amigo, padre que en estos momentos dificiles necesita de nosotros",
       "vendidos": 10,
-      "id": 2
+      "premios": [
+        "Un Barrilito de Heineken",
+        "Un Chromecast",
+        "Una Jarra electrica"
+      ]
     }
   ]
 }
